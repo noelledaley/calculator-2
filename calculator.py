979ed57e8de43003b2edbe93acfa5156fb49ea27
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 18:05:10 -0700

    Trying to validate user data; broke stuff

--- calculator.py
+++ calculator.py
@@ -12,38 +12,52 @@ def calculate_stuff():
 
     Takes no arguments and returns nothing.
     """
-    exit_token = None
-    while exit_token != 'q':
+    input_string = 'start'
+    while 'q' not in input_string:
         # Runs program until the user types in 'q'.
 
         input_string = raw_input("> ")
-        input_list = input_string.split(" ")
-        
-        # Converts user input into integers.
+        print input_string
+
+        input_list = input_string.split(" ")        
+
         i = 1
         while i < len(input_list):
             input_list[i] = int(input_list[i])
             i += 1
-
+        list_checker(input_list)
         
-        if input_list[0] == "+":
+def list_checker(input_list):
+
+    if input_list[0] == type(str) and input_list[1:] == type(int):
+        return math_function_picker(input_list)
+    else:
+        # import pdb; pdb.set_trace()
+        print input_list #figure out what the types of the elements of htis list are.
+        return
+
+
+def math_function_picker(input_list):
+
+        # Converts user input into integers.
+
+
+        if input_list[0] == "+" and len(input_list) == 3:
             print add(input_list[1], input_list[2])
-        elif input_list[0] == "-":
+        elif input_list[0] == "-" and len(input_list) == 3:
             print subtract(input_list[1], input_list[2])
-        elif input_list[0] == "*":
+        elif input_list[0] == "*" and len(input_list) == 3:
             print multiply(input_list[1], input_list[2])
-        elif input_list[0] == "/":
+        elif input_list[0] == "/" and len(input_list) == 3:
             print divide(input_list[1], input_list[2])
-        elif input_list[0] == "square":
+        elif input_list[0] == "square" and len(input_list) == 2:
             print square(input_list[1])
-        elif input_list[0] == "cube":
+        elif input_list[0] == "cube" and len(input_list) == 2:
             print cube(input_list[1])
-        elif input_list[0] == "pow":
+        elif input_list[0] == "pow" and len(input_list) == 3:
             print power(input_list[1], input_list[2])
-        elif input_list[0] == "mod":
+        elif input_list[0] == "mod" and len(input_list) == 3:
             print mod(input_list[1], input_list[2])
-        elif 'q' in input_string:
-            break
         else:
             print "I don't understand"
 
